71d119cc8ddfca5ab753bdbe91964543ff0e85c4
Lucie Genevey <l.genevey@gmail.com>
2023-02-20 23:11:30 +0100

    main updated

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 16:57:36 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/02/20 17:46:45 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/02/20 21:37:10 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	
-
-int	main(int argc, char *argv)
+int	main()
 {
-	for (int i = 0; i < argc; i++)
+/* 	for (int i = 0; i < argc; i++)
 	{
 		char* p = argv[i];
 		while (*p)
@@ -25,6 +23,13 @@ int	main(int argc, char *argv)
 			p++;
 		}
 		std::cout << argv[i] << std::endl;
-	}
+	} */
+
+	char	buff[512];
+
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Input a word: ";
+	std::cin >> buff;
+	std::cout << "You entered: [" << buff << "]" << std::endl;
 	return (0);
 }

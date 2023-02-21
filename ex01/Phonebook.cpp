b3fe6d7e47a83eba43675e128a7d3146600fb513
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-02-21 21:44:48 +0100

    truc

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 18:42:28 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/02/21 18:42:31 by lgenevey         ###   ########.fr       */
+/*   Created: 2023/02/21 18:42:35 by lgenevey          #+#    #+#             */
+/*   Updated: 2023/02/21 18:42:36 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; ++i)
-		{
-			std::string str(argv[i]);
-			for (unsigned long j = 0; j < str.length(); ++j)
-			{
-				str[j] = std::toupper(str[j]);
-			}
-			std::cout << str;
-		}
-		std::cout << std::endl;
-	}
-	return (0);
-}
--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 18:42:28 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/02/21 18:42:31 by lgenevey         ###   ########.fr       */
+/*   Created: 2023/02/21 18:51:24 by lgenevey          #+#    #+#             */
+/*   Updated: 2023/02/21 20:28:55 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
 #include <iostream>
 
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; ++i)
-		{
-			std::string str(argv[i]);
-			for (unsigned long j = 0; j < str.length(); ++j)
-			{
-				str[j] = std::toupper(str[j]);
-			}
-			std::cout << str;
-		}
-		std::cout << std::endl;
-	}
-	return (0);
-}
+class Phonebook{
+
+public:
+
+private:
+
+
+
+};
+
+class Contact{
+private:
+	std::string contact;
+};
+
+#endif

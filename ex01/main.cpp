52061e68b8e5db44346c21df089927fa4d2a5603
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-02-26 18:31:56 +0100

    Display commands, super important

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:58:30 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/02/26 14:17:54 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/02/26 18:30:32 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,18 @@ int	main(void)
 
 	while (ret)
 	{
+		std::cout << "-------------------------------------------------" << std::endl;
+		std::cout << "| Please enter one of these following command:	|" << std::endl;
+		std::cout << "|                                              	|" << std::endl;
+		std::cout << "| ADD		: Add a new contact		|" << std::endl;
+		std::cout << "| SEARCH	: Display and search a contact	|" << std::endl;
+		std::cout << "| EXIT		: Exit the programm		|" << std::endl;
+		std::cout << "-------------------------------------------------" << std::endl;
 		std::getline(std::cin, command);
 		if (command == "ADD")
 			annuaire.addContact();
-		else if (command == "SEARCH")
-			displayPhonebook();
+		// else if (command == "SEARCH")
+		// 	displayPhonebook();
 			// afficher l'annuaire
 			// demander quel contact afficher
 		else if (command == "EXIT")

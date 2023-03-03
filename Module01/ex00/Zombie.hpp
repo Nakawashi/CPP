dd7f9baaf4648e94b55ad983915289958a6644bc
Lucie Genevey <l.genevey@gmail.com>
2023-03-03 10:54:22 +0100

    create Module01 folder

---
+++ Module01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	_name;
+
+
+	public:
+		Zombie(void);
+		~Zombie(void);
+		void	announce(void);
+};
+
+#endif

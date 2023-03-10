c0b7b48507fe6fe37051ae4e5242f78d8e7380dc
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-03-10 17:16:53 +0100

    fix eof infinite loop  after gabriel's evaluation

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:58:30 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/03/10 13:03:53 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/03/10 17:15:37 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,23 @@ int	main(void)
 	std::string	command;
 	Phonebook	phonebook;
 	bool		ret = true;
+
 	do
 	{
 		phonebook.displayPrompt();
 		std::getline(std::cin, command);
+		if (std::cin.eof())
+		{
+			std::cin.clear();
+			clearerr(stdin);
+			continue ;
+		}
+		if (command == "EXIT" || command == "exit")
+			ret = false;
 		if (command == "ADD")
 			phonebook.addContact();
 		else if (command == "SEARCH")
 			phonebook.searchContact();
-		else if (command == "EXIT" || command == "exit")
-			ret = false;
 	} while (ret);
 	return 0;
 }

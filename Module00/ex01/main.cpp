145abfc7179699cde3b97f97dae59024d8d59b71
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-03-13 17:36:19 +0100

    usage of .good and  clear()

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,12 +6,22 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:58:30 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/03/10 17:15:37 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/03/13 17:35:41 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
+#include <iostream>
 
+
+/*
+	Par défaut, clear() réinitialise les indicateurs d'état
+		goodbit
+		eofbit
+		failbit
+	de l'objet.
+	Cela signifie que la méthode remet l'objet dans un état valide, prêt à être réutilisé.
+*/
 int	main(void)
 {
 	std::string	command;
@@ -22,8 +32,9 @@ int	main(void)
 	{
 		phonebook.displayPrompt();
 		std::getline(std::cin, command);
-		if (std::cin.eof())
+		if (!std::cin.good())
 		{
+			std::cout << "Error: bad input" << std::endl;
 			std::cin.clear();
 			clearerr(stdin);
 			continue ;
@@ -34,6 +45,7 @@ int	main(void)
 			phonebook.addContact();
 		else if (command == "SEARCH")
 			phonebook.searchContact();
+		std::cin.clear();
 	} while (ret);
 	return 0;
 }

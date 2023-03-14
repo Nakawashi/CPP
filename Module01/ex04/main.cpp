9b8d14448cbdf004578c376dc70ce6f8fd2990ae
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-03-14 15:10:55 +0100

    read from ifs and save it in str OK

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -3,35 +3,27 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	std::string str = "";
+
 	if (argc < 4)
 	{
 		std::cout << "Error: make a function for good usage of arguments" << std::endl;
 		return (1);
 	}
 
-	// std::ifstream ifs(argv[1]); // ifs : input file stream
-	std::ifstream ifs("test1"); // ifs : input file stream
-	ifs.is_open(); // check if the file is open or exists
-	// utiliser getline comme dans phonebook pour convertir le stream en string, vu qu'on peut pas revenir en arriere dans le stream, comme le read de GNL
-
-	// convertir ifs en string pour utiliser les fonctions de string, via getline()
-
-	// std::string s1 = argv[2];
-	std::string s1 = "";
-	// std::string s2 = argv[3];
-	std::string s2 = "";
-
-	ifs >> s1 >> s2;
-
-	std::cout << s1 << " " << s2 << std::endl;
-	ifs.close();
-
-	std::ofstream ofs("test2");
-	ofs << "ce que je mets dans le fichier" << std::endl;
-	ofs.close();
-
+	std::ifstream ifs(argv[1]); // ifs : input file stream
+	if (ifs.is_open()) // returns true if the file is open and associated with this stream object
+	{
+		// utiliser getline comme dans phonebook pour convertir le stream en string, vu qu'on peut pas revenir en arriere dans le stream, comme le read de GNL
+		std::getline(ifs, str);
+		std::cout << str << std::endl;
+		ifs.close();
+	}
+	else
+	{
+		std::cout << "File " << argv[1] << " does not exists, or error occured." << std::endl;
+		return (1);
+	}
 
 	return (0);
 }

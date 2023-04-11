b47f2d7b0f506786c8df73078e1611517a54774e
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-04-11 21:35:20 +0200

    check values if we try to go under zero

--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -8,7 +8,7 @@ int	main(void)
 	nouveau.setAttackDmg(9);
 	nouveau.attack(test.getName());
 	nouveau.beRepaired(2);
-	nouveau.takeDamage(10);
+	nouveau.takeDamage(13);
 
 	std::cout << nouveau << std::endl;
 	return 0;

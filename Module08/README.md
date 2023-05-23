58977e54e3c0a6e359730be1ae3a5eaaa06fede6
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-05-23 21:10:42 +0200

    start 08

---
+++ Module08/README.md
@@ -0,0 +1,11 @@
+# Objectifs
+
+*Containers templates, itérateurs, algorithmes, version 7*
+
+Merci a cette personne d'avoir réalisé ce wiki incroyable : [Wiki de qingqingqingli](https://github.com/qingqingqingli/CPP/tree/main/module08)
+
+## Ex00
+
+## Ex01
+
+## Ex02
---
+++ Module08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 18:04:07 by lgenevey          #+#    #+#             */
+/*   Updated: 2023/05/23 18:32:59 by lgenevey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <exception>
+
+template<typename T = int>
+void	easyfind(T container, int what)
+{
+	
+}
+
+#endif

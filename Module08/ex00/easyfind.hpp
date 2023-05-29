77a25933302ba3555023386b3839393d1a36fc42
Lucie Genevey <l.genevey@gmail.com>
2023-05-29 23:44:12 +0200

    ex00 done

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:04:07 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/05/23 18:32:59 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/05/29 23:42:28 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@
 # include <iostream>
 # include <exception>
 
-template<typename T = int>
-void	easyfind(T container, int what)
+/*
+	typename : informs the compilator that T::iterator is a type
+*/
+template<typename T>
+typename T::iterator	easyfind(T &container, int what)
 {
-	
+	for (typename T::iterator it = container.begin(); it != container.end(); ++it)
+	{
+		if (*it == what)
+			return it;
+	}
+	return container.end();
 }
 
 #endif
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -3,17 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:04:01 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/05/23 18:05:29 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/05/29 23:43:18 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <algorithm>
+#include <vector>
 #include "easyfind.hpp"
 
 int	main(void)
 {
+	int	myints[] = {2, 5, 87, 453};
+	std::vector<int> v(myints, myints + 4);
+	std::vector<int>::iterator it;
 
+	it = easyfind(v, 2);
+	if (it != v.end())
+		std::cout << "number found" << std::endl;
+	else
+		std::cout << "number not found" << std::endl;
+
+
+	it = easyfind(v, 20);
+	if (it != v.end())
+		std::cout << "number found" << std::endl;
+	else
+		std::cout << "number not found" << std::endl;
 	return 0;
 }

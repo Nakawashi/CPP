4388cdb8440cc67e4b6c0507b2e50ad99b55f197
Lucie Genevey <l.genevey@gmail.com>
2023-05-30 14:31:19 +0200

    ex00 ok use find()

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,17 +6,19 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:04:07 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/05/29 23:45:19 by nakawashi        ###   ########.fr       */
+/*   Updated: 2023/05/30 14:28:28 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+
 /*
 	typename : informs the compilator that T::iterator is a type
 */
-template<typename T>
+/* template<typename T>
 typename T::iterator	easyfind(T &container, int what)
 {
 	for (typename T::iterator it = container.begin(); it != container.end(); ++it)
@@ -24,6 +26,15 @@ typename T::iterator	easyfind(T &container, int what)
 		if (*it == what)
 			return it;
 	}
+	return container.end();
+} */
+
+template<typename T>
+typename T::iterator	easyfind(T &container, int what)
+{
+	typename T::iterator it = find(container.begin(), container.end(), what);
+	if (*it == what)
+		return it;
 	return container.end();
 }
 

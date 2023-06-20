aa5d82e586f47267c308fd6d31fdea23701628f1
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-06-20 16:33:31 +0200

    ex00 OK

--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -6,29 +6,9 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:25:17 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/06/19 19:26:51 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/06/20 16:33:16 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-	1. verifier l'input :
-		le second doit etre un fichier
-		le fichier doit exister et pouvoir etre ouvert voir module01
-	2. Si ok, stocker les valeurs dans deux variables
-		dateValidation (YY-MM-DD)
-		valueValidation (float or integer)
-	3. Creer un objet de classe BitcoinExchange
-		constructeur : initialise map avec mon fichier de datas pour laisser la possibilite d'avoir d'autres données
-		fonctions membre : find date avec lower_bound and returns value * exchange.
-			if no exact date found :
-				l'iterateur retourne par lower_bound -- sinon il retourne le suivant
-
-
-//stringstream
-//mktime
-//t_time
-//map::lower_bound
-*/
 
 #include "BitcoinExchange.hpp"
 

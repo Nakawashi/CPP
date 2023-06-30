bf671016aaf4b2e851ee5ee1fd87386c0af14765
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-06-30 09:05:52 +0200

    s2 seems OK

--- examens/s2/ASpell.cpp
+++ examens/s2/ASpell.cpp
@@ -6,7 +6,10 @@ ASpell::ASpell(std::string name, std::string effects)
 : _name(name), _effects(effects) {}
 
 ASpell::ASpell(const ASpell& src)
-: _name(src._name), _effects(src._effects) {}
+: _name(src._name), _effects(src._effects)
+{
+	*this = src;
+}
 
 ASpell::~ASpell() {}
 
